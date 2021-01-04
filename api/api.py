306c80fa4b30b9d8f8a249650b8230bd20030ed5
Sam Bowman <sam@bowmans.site>
2021-01-04 16:44:36 +0000

    created db class templates

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ import time, os
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
@@ -10,6 +11,16 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 CORS(app)
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    admin = db.Column(db.Boolean)
+
 
 @app.route('/time')
 def get_current_time():
