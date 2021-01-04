c69e996c7bd0bd10a5c91ef9f419b46a8213c5b3
Sam Bowman <sam@bowmans.site>
2021-01-04 16:40:34 +0000

    added reading of db location to backend

--- api/api.py
+++ api/api.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 app  =  Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
 CORS(app)
 
 @app.route('/time')
