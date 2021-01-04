8050c3e0a0623a93a7538663b9f7a3264dfe5183
Sam Bowman <sam@bowmans.site>
2021-01-04 17:40:34 +0000

    added login function to api backend

--- api/api.py
+++ api/api.py
@@ -113,8 +113,8 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
 
     if check_password_hash(user.password, auth.password):
-        token =jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('UTF-8')})
+        token = jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
     
