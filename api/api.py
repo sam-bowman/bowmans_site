47227cc77c06422b6e7e73a5c461ff78ec5a1ced
Sam Bowman <sam@bowmans.site>
2021-01-04 17:33:46 +0000

    added login function to api backend

--- api/api.py
+++ api/api.py
@@ -100,23 +100,23 @@ def delete_user(public_id):
 
     return jsonify({'message': 'User Deleted!'})
 
-# @app.route('/login')
-# def login():
-#     auth = request.authorization
+@app.route('/login')
+def login():
+    auth = request.authorization
 
-#     if not auth or not auth.username or not auth.password:
-#         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
 
-#     user = User.query.filter_by(name=auth.username).first()
+    user = User.query.filter_by(name=auth.username).first()
     
-#     if not user:
-#         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
 
-#     if check_password_hash(user.password, auth.password):
-#         token =jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-#         return jsonify({'token': token.decode('UTF-8')})
+    if check_password_hash(user.password, auth.password):
+        token =jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
 
-#     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic Realm="Login Required!"'})
     
 if __name__ == '__main__':
     app.run()
