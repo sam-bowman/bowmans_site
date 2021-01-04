3e5684cd334b23f171c124c546718bdcd20f2da3
Sam Bowman <sam@bowmans.site>
2021-01-04 17:06:55 +0000

    added get_all_users function to backend api

--- api/api.py
+++ api/api.py
@@ -29,7 +29,21 @@ def get_current_time():
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return ''
+    
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+
+        output.apped(user_data)
+
+    return jsonify({'users': output})
 
 @app.route('/user/<user_id>', methods=['GET'])
 def get_user():
