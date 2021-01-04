a6fed4c5664ba36b736e1f72af9a1bd3efd75a10
Sam Bowman <sam@bowmans.site>
2021-01-04 16:30:06 +0000

    checking environ variables

--- api/api.py
+++ api/api.py
@@ -1,13 +1,14 @@
-import time
+import time, os
 from flask import Flask
 from flask_cors import CORS
 
 app  =  Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 CORS(app)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {'time': time.time(), "secret_key": os.environ.get("SECRET_KEY")}
 
 if __name__ == '__main__':
     app.run()
